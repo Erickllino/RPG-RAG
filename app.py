646e5d01a829f8a1b13162f07525157b7bb18e15
Erickllino <erickvrc@outlook.com>
2026-03-04 20:05:43 -0300

    update README.md for hf

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 # ponta de entrada do gradio
+
+
+import gradio as gr
+
+def my_function(user_input):
+    return "You said: " + user_input
+
+demo = gr.Interface(
+    fn=my_function,       # the function to call
+    inputs="text",        # what the user types
+    outputs="text",       # what gets shown back
+)
+
+demo.launch()
